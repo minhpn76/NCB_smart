41f5a49cf822fbba7e20d7172ffbac12f5a39dc3
minhpn77 <phamngocminh7694@gmail.com>
2019-12-03 14:19:53 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://113.20.126.169:8081/NCB-smart'
-  apiUrl: 'https://cmsmobile.ncb-bank.vn:8443/NCB-smart'
+  apiUrl: 'http://113.20.126.169:8081/NCB-smart'
+  // apiUrl: 'https://cmsmobile.ncb-bank.vn:8443/NCB-smart'
 };
 
 /*
