44f45bc51b21428930f8f3a17dc8cb0bad1eb2da
Xuân Tiến Nguyễn <xuantjen410@gmail.com>
2020-08-07 11:35:37 +0700

    save

--- src/app/layout/qr-server/list-qr/list-qr.component.ts
+++ src/app/layout/qr-server/list-qr/list-qr.component.ts
@@ -22,7 +22,7 @@ export class ListQrComponent implements OnInit {
         size: 10,
         page: 0,
         previous_page: 0
-    }
+    };
     isProcessLoad: any = 0;
     totalSearch: any = 0;
     order = 'titile';
@@ -48,9 +48,18 @@ export class ListQrComponent implements OnInit {
 
     listData = [];
 
-    onSearch(search) {
-        alert(search);
-    }
+    keyDownFunction(event) {
+        if (event.keyCode === 13) {
+          this.getListData(this.search);
+        }
+      }
+    onSearch(payload) {
+        // payload.page = 0;
+        if (payload.title !== '' || payload.status !== '') {
+          payload.page = 0;
+        }
+        this.getListData(payload);
+      }
     ngOnInit() {
         this.getListData(this.search);
     }
@@ -100,19 +109,18 @@ export class ListQrComponent implements OnInit {
             cancelButtonText: 'Không, trở lại',
         }).then((result) => {
             if (result.value) {
-                // this.ncbService.deleteUser(id).then((res) => {
-                //     if (res.json().code === "00") {
-                //         Swal.fire(
-                //             "Đã xoá!",
-                //             "Dữ liệu đã xoá hoàn toàn.",
-                //             "success"
-                //         );
-                //         this.onSearch(this.search);
-                //     } else {
-                //         this.toastr.error("Xoá dữ liệu thất bại", "Thất bại");
-                //     }
-                // });
-                alert('Thành công');
+                this.ncbService.deleteUser(id).then((res) => {
+                    if (res.json().code === '00') {
+                        Swal.fire(
+                            'Đã xoá!',
+                            'Dữ liệu đã xoá hoàn toàn.',
+                            'success'
+                        );
+                        this.onSearch(this.search);
+                    } else {
+                        this.toastr.error('Xoá dữ liệu thất bại', 'Thất bại');
+                    }
+                });
             } else if (result.dismiss === Swal.DismissReason.cancel) {
                 Swal.fire('Huỷ bỏ', 'Dữ liệu được bảo toàn :)', 'error');
             }
