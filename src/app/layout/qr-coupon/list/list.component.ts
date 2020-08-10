ba057b7640007a712d378eb20bc01f0c7db4a670
Xuân Tiến Nguyễn <xuantjen410@gmail.com>
2020-08-10 17:19:35 +0700

    save

--- src/app/layout/qr-coupon/list/list.component.ts
+++ src/app/layout/qr-coupon/list/list.component.ts
@@ -2,26 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { NCBService } from '../../../services/ncb.service';
 import { ToastrService } from 'ngx-toastr';
+import { Helper } from '../../../helper';
+import { DebugHelper } from 'protractor/built/debugger';
 
 @Component({
     selector: 'app-list',
     templateUrl: './list.component.html',
     styleUrls: ['./list.component.scss'],
-    providers: [NCBService]
+    providers: [NCBService, Helper],
 })
 export class ListComponent implements OnInit {
     constructor(
         private ncbService: NCBService,
         public toastr: ToastrService,
-    ) {
-
-    }
+        public helper: Helper
+    ) {}
     search: any = {
         name: '',
         status: '',
+        approveStatus: '',
+        discountType: '',
+        description: '',
+        // startDate: '',
+        // endDate: '',
         size: 10,
         page: 0,
-        previous_page: 0
+        previous_page: 0,
     };
     isProcessLoad: any = 0;
     totalSearch: any = 0;
@@ -37,60 +43,107 @@ export class ListComponent implements OnInit {
             code: '',
         },
         {
-            name: 'Disable',
+            name: 'Kích hoạt',
             code: 'A',
         },
         {
-            name: 'Enable',
+            name: 'Chưa kích hoạt',
             code: 'D',
-        }
+        },
+    ];
+
+    listStatusApproved: any = [
+        {
+            name: 'Tất cả',
+            code: '',
+        },
+        {
+            name: 'Chưa phê duyệt',
+            code: '1',
+        },
+        {
+            name: 'Phê duyệt',
+            code: '0',
+        },
+    ];
+
+    listDiscounts: any = [
+        {
+            name: 'Tất cả',
+            code: '',
+        },
+        {
+            name: 'Phần trăm',
+            code: '1',
+        },
+        {
+            name: 'Giá tiền',
+            code: '0',
+        },
     ];
 
     listData = [];
 
     keyDownFunction(event) {
         if (event.keyCode === 13) {
-          this.getListData(this.search);
+            this.getListData(this.search);
         }
-      }
+    }
     onSearch(payload) {
         // payload.page = 0;
-        if (payload.name !== '' || payload.status !== '') {
-          payload.page = 0;
+        if (
+            payload.name !== '' ||
+            payload.status !== '' ||
+            payload.approveStatus !== '' ||
+            payload.discountType !== '' ||
+            payload.startDate !== '' ||
+            payload.endDate !== ''
+        ) {
+            payload.page = 0;
         }
         this.getListData(payload);
-      }
+    }
+    onQuickSearch(payload) {
+        if (payload.name !== '' || payload.description !== '') {
+            payload.page = 0;
+        }
+        this.getListData(payload);
+    }
+
     ngOnInit() {
         this.getListData(this.search);
     }
 
-    getListData (params: any) {
+    getListData(params: any) {
         this.listData = [];
         this.isProcessLoad = 1;
         // xu ly
-        this.ncbService.getListQRCoupon(params).then((result) => {
-        setTimeout(() => {
-            const body = result.json().body;
-            this.listData = body.content;
-            this.totalSearch = body.totalElements;
-            this.isProcessLoad = 0;
-        }, 300);
-        }).catch(err => {
-        this.isProcessLoad = 0;
-        this.listData = [];
-        this.totalSearch = 0;
-        // this.toastr.error('Không lấy được danh sách dữ liệu. Vui lòng liên hệ khối Công nghệ để được hỗ trợ', 'Lỗi hệ thống!');
-        });
+        this.ncbService
+            .getListQRCoupon(params)
+            .then((result) => {
+                setTimeout(() => {
+                    const body = result.json().body;
+                    this.listData = body.content;
+                    this.totalSearch = body.totalElements;
+                    this.isProcessLoad = 0;
+                }, 300);
+            })
+            .catch((err) => {
+                this.isProcessLoad = 0;
+                this.listData = [];
+                this.totalSearch = 0;
+                // this.toastr.error('Không lấy được danh sách dữ liệu. Vui lòng liên hệ khối Công nghệ để được hỗ trợ', 'Lỗi hệ thống!');
+            });
     }
     loadPage(page: number) {
         const page_number = page - 1;
         if (page_number !== this.search.previous_page) {
-          this.search.page = page_number;
-          this.search.previous_page = page_number;
-          this.getListData(this.search);
-          this.search.page = page;
+            this.search.page = page_number;
+            this.search.previous_page = page_number;
+            this.getListData(this.search);
+            this.search.page = page;
         }
-      }
+    }
     setOrder(value: string) {
         if (this.order === value) {
             this.reverse = !this.reverse;
