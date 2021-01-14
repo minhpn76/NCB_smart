c9979288825e19d3133f11a841c318ec3c46fb80
tiennx <xuantjen410@gmail.com>
2021-01-14 15:34:59 +0700

    save

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://10.1.62.31:443/NCB-smart'
-  apiUrl: 'http://192.168.53.127:443/NCB-smart'
+  apiUrl: 'http://10.1.62.31:443/NCB-smart'
+  // apiUrl: 'http://192.168.53.127:443/NCB-smart'
   // apiUrl: 'https://cmsmobile.ncb-bank.vn:8443/NCB-smart'
 };
 
